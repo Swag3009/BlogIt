75cb67428778ac9fa83aa1d4a55375b5323b80e9
Swagatika Mohapatra <mswagatika3009@gmail.com>
2025-06-27 11:24:14 +0530

    Implemented validation in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  MAX_TITLE_LENGTH = 125
+  MAX_DESCRIPTION_LENGTH = 10_000
+
+  validates :title,
+    presence: true,
+    length: {maximum: MAX_TITLE_LENGTH}
+
+  validates :description,
+    presence: true,
+    length: {maximum: MAX_DESCRIPTION_LENGTH}
+
+  validates_inclusion_of :is_bloggable,
+    in: [true, false]
 end
