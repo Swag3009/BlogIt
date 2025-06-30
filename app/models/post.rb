bb820bd5031c218b5c7ce2a5fd8661bf8a7dfd15
Swagatika Mohapatra <mswagatika3009@gmail.com>
2025-06-30 17:00:49 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   MAX_TITLE_LENGTH = 125
   MAX_DESCRIPTION_LENGTH = 10_000
 
   validates :title,
     presence: true,
-    length: {maximum: MAX_TITLE_LENGTH}
+    length: { maximum: MAX_TITLE_LENGTH }
 
   validates :description,
     presence: true,
-    length: {maximum: MAX_DESCRIPTION_LENGTH}
+    length: { maximum: MAX_DESCRIPTION_LENGTH }
 
   validates_inclusion_of :is_bloggable,
     in: [true, false]
