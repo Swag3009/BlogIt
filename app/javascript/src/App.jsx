28f3abe772e56c4d175a5009faf276124b6ee85a
Swagatika Mohapatra <mswagatika3009@gmail.com>
2025-07-02 23:58:37 +0530

    Created component file structure

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
+import Blogs from "./components/Blogs";
+
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" render={() => <h1>Home</h1>} />
-      <Route exact path="/about" render={() => <h1>About</h1>} />
+      <Route exact component={Blogs} path="/blogs" />
     </Switch>
   </Router>
 );
