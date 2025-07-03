9b251f2eaa0a3d4225539bdd435a4cae67e9f69e
Swagatika Mohapatra <mswagatika3009@gmail.com>
2025-07-03 08:16:55 +0530

    NeetoUI setup

--- app/javascript/src/components/Blogs/index.jsx
+++ app/javascript/src/components/Blogs/index.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-const Blogs = () => <div>Blogs</div>;
+import { Button } from "@bigbinary/neetoui";
+
+const Blogs = () => (
+  <div>
+    <h1>Hello</h1>
+    <Button label="Button" style="primary" onClick={function noRefCheck() {}} />
+  </div>
+);
 
 export default Blogs;
