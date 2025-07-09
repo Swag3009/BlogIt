ead3679c7cac9c792becfe204cf100c726edb080
Swagatika Mohapatra <mswagatika3009@gmail.com>
2025-07-09 15:56:57 +0530

    Created container

--- app/javascript/src/components/Blog/List.jsx
+++ app/javascript/src/components/Blog/List.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 import { NoData, Typography } from "@bigbinary/neetoui";
+import { PageLoader, ErrorMessage, Container } from "components/common";
 import { isEmpty } from "ramda";
 import { useTranslation } from "react-i18next";
 
 import BlogCard from "./Card";
 
 import { useFetchPosts } from "../../hooks/reactQuery/usePostsApi";
-import { PageLoader, ErrorMessage } from "../common";
-import NavBar from "../NavBar";
 
 const Blogs = () => {
   const { t } = useTranslation();
@@ -19,23 +18,20 @@ const Blogs = () => {
   if (isError) return <ErrorMessage />;
 
   return (
-    <div className="flex">
-      <NavBar />
-      <main className="ml-16 w-full p-6">
-        <Typography className="mb-8" style="h1">
-          {t("title.blogPost")}
-        </Typography>
-        {isEmpty(posts) ? (
-          <NoData title="No blog posts available." />
-        ) : (
-          <div className="space-y-6">
-            {posts.map(({ id, title, description, created_at: createdAt }) => (
-              <BlogCard key={id} {...{ title, description, createdAt }} />
-            ))}
-          </div>
-        )}
-      </main>
-    </div>
+    <Container className="ml-16 w-full p-6">
+      <Typography className="mb-8" style="h1">
+        {t("title.blogPost")}
+      </Typography>
+      {isEmpty(posts) ? (
+        <NoData title="No blog posts available." />
+      ) : (
+        <div className="space-y-6">
+          {posts.map(({ id, title, description, created_at: createdAt }) => (
+            <BlogCard key={id} {...{ title, description, createdAt }} />
+          ))}
+        </div>
+      )}
+    </Container>
   );
 };
 
