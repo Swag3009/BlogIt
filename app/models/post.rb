9fab0d996f7e0e5b0afa0bda354ba39cb35ebe22
Swagatika Mohapatra <mswagatika3009@gmail.com>
2025-07-11 06:51:51 +0530

    Database design

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@
 class Post < ApplicationRecord
   MAX_TITLE_LENGTH = 125
   MAX_DESCRIPTION_LENGTH = 10_000
+  belongs_to :user
+  belongs_to :organization
+  has_and_belongs_to_many :categories
 
   validates :title,
     presence: true,
