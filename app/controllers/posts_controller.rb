317809c6e9462a9a038006370092138baaee56fc
Swagatika Mohapatra <mswagatika3009@gmail.com>
2025-07-21 15:44:29 +0530

    Added pundit post policy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,20 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  after_action :verify_authorized, except: %i[index my_posts]
+  after_action :verify_policy_scoped, only: %i[index my_posts]
+
   def index
-    current_org_id = current_user.organization_id
-    posts = Post
-      .includes(:user, :categories)
-      .joins(:user)
-      .where(users: { organization_id: current_org_id })
-      .order(updated_at: :desc)
+    posts = policy_scope(Post)
+    posts = filter_by_categories(posts)
+    @paginated_posts = paginate_posts(posts)
+    render
+  end
 
-    if params[:categories].present?
-      category_ids = params[:categories].split(",").map(&:to_i)
-      posts = posts
-        .joins(:categories)
-        .where(categories: { id: category_ids })
-        .distinct
-    end
-
-    @paginated_posts = posts.paginate(page: params[:page], per_page: params[:per_page] || 5)
+  def my_posts
+    posts = PostPolicy::Scope.new(current_user, Post).personal
+    posts = filter_by_categories(posts)
+    @paginated_posts = paginate_posts(posts)
     render
   end
 
@@ -25,16 +22,30 @@ class PostsController < ApplicationController
     post = Post.new(post_params)
     post.user = current_user
     post.organization = current_user.organization
+    authorize post
     post.save!
     render_notice(t("successfully_created", entity: "Post"))
   end
 
   def show
     @post = Post.includes(:user, :categories).find_by!(slug: params[:slug])
+    authorize @post
     render
   end
 
   private
+
+    def filter_by_categories(posts)
+      return posts unless params[:categories].present?
+
+      category_ids = params[:categories].split(",").map(&:to_i)
+      posts.joins(:categories).where(categories: { id: category_ids }).distinct
+    end
+
+    def paginate_posts(posts)
+      posts.order(updated_at: :desc)
+        .paginate(page: params[:page], per_page: params[:per_page] || 5)
+    end
 
     def post_params
       params.require(:post).permit(:title, :description, :is_bloggable, category_ids: [])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@
 
 Rails.application.routes.draw do
   constraints(lambda { |req| req.format == :json }) do
-    resources :posts, only: %i[index create show], param: :slug
+    resources :posts, only: %i[index create show], param: :slug do
+      collection do
+        get :my_posts
+      end
+    end
+
     resources :categories, only: %i[index create]
     resources :organizations, only: :index
     resources :users, only: :create
